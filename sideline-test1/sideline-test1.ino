37314552d208152ddaf34e3b236ad6fedf76880d
KevinM-NEDC <55644057+KevinM-NEDC@users.noreply.github.com>
2019-11-30 19:04:38 -0800

    Update sideline-test1.ino

--- sideline-test1/sideline-test1.ino
+++ sideline-test1/sideline-test1.ino
@@ -1,6 +1,6 @@
-
 const int Button = 9;
-const int relayModule = 6;
+const int relayModule1 = 6;
+const int relayModule2 = 7;
 
 
 int buttonState = 0;
@@ -10,9 +10,11 @@ int motorEnabled = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode (relayModule, OUTPUT);
+  pinMode (relayModule1, OUTPUT);
+  pinMode(relayModule2, OUTPUT);
   pinMode(Button, INPUT_PULLUP);
-  digitalWrite (relayModule, LOW);
+  digitalWrite (relayModule1, LOW);
+  digitalWrite (relayModule2, LOW);
 }
 
 void loop() {
@@ -26,10 +28,12 @@ void loop() {
   }
 
   if (motorEnabled == 1){
-    digitalWrite (relayModule, HIGH);
+    digitalWrite (relayModule1, HIGH);
+    digitalWrite (relayModule2, HIGH);
   }
   else {
-    digitalWrite (relayModule, LOW);
+    digitalWrite (relayModule1, LOW);
+    digitalWrite (relayModule2, LOW);
   }
 
   prevButtonState = buttonState;
